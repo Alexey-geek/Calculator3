c63d65389ce4368643b476056eea85099942f110
Alexey-geek <samoiloff.alex@gmail.com>
2021-07-30 18:25:07 +0300

    Поставлены прослушки на все кнокпки

--- app/src/main/java/com/calculator/MainActivity.java
+++ app/src/main/java/com/calculator/MainActivity.java
@@ -1,14 +1,55 @@
 package com.calculator;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity{
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initButton();
+
+    }
+    @Override
+    public void onClick(View view) {
+
+    }
+    private void initButton() {
+        Button buttonOne = findViewById(R.id.buttonOne);
+        Button buttonTwo = findViewById(R.id.buttonTwo);
+        Button buttonThree = findViewById(R.id.buttonThree);
+        Button buttonFour = findViewById(R.id.buttonFour);
+        Button buttonEqual = findViewById(R.id.buttonEqual);
+        Button buttonFive = findViewById(R.id.buttonFive);
+        Button buttonSix = findViewById(R.id.buttonSix);
+        Button buttonSeven = findViewById(R.id.buttonSeven);
+        Button buttonEight = findViewById(R.id.buttonEight);
+        Button buttonNine = findViewById(R.id.buttonNine);
+        Button buttonZero = findViewById(R.id.buttonZero);
+        Button buttonSum = findViewById(R.id.buttonSum);
+        Button buttonDif = findViewById(R.id.buttonDif);
+        Button buttonDiv = findViewById(R.id.buttonDiv);
+        Button buttonCom = findViewById(R.id.buttonCom);
+        Button buttonClear = findViewById(R.id.buttonClear);
+        buttonOne.setOnClickListener(this);
+        buttonTwo.setOnClickListener(this);
+        buttonThree.setOnClickListener(this);
+        buttonFour.setOnClickListener(this);
+        buttonFive.setOnClickListener(this);
+        buttonSix.setOnClickListener(this);
+        buttonSeven.setOnClickListener(this);
+        buttonEight.setOnClickListener(this);
+        buttonNine.setOnClickListener(this);
+        buttonZero.setOnClickListener(this);
+        buttonSum.setOnClickListener(this);
+        buttonDif.setOnClickListener(this);
+        buttonDiv.setOnClickListener(this);
+        buttonCom.setOnClickListener(this);
+        buttonClear.setOnClickListener(this);
+        buttonEqual.setOnClickListener(this);
     }
 }
